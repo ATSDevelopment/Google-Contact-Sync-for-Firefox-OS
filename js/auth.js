b6b90ef9609bc08ad93b7b07c81d96bca40ed9af
Carlos Souza Rodrigues Filho <car.s.r.f@gmail.com>
2015-09-29 02:26:40 -0300

    Atualização, sistema ja está sincronizando os contatos

--- js/auth.js
+++ js/auth.js
@@ -1,15 +1,14 @@
-LINK = "https://accounts.google.com/o/oauth2/auth";
-RESPONSE_TYPE = "?response_type=none";
-CLIENT_ID = "&client_id=367393827472-dm44e1puvndsd9je57ceatdldd05sjju.apps.googleusercontent.com";
-REDIRECT_URI = "&redirect_uri=http://gsync.redir.com/index.html";
-SCOPE = "&scope=http://www.google.com/m8/feeds";
+$(document).ready(function(){
+	$("#btn").click(function(){
+		var LINK = "https://accounts.google.com/o/oauth2/auth";
+		var RESPONSE_TYPE = "?response_type=token";
+		var CLIENT_ID = "&client_id=367393827472-dm44e1puvndsd9je57ceatdldd05sjju.apps.googleusercontent.com";
+		var REDIRECT_URI = "&redirect_uri=http://gsync.app/auth-c.html";
+		var SCOPE = "&scope=http://www.google.com/m8/feeds";
 
-URL = LINK+RESPONSE_TYPE+CLIENT_ID+REDIRECT_URI+SCOPE;
+		var URL = LINK+RESPONSE_TYPE+CLIENT_ID+REDIRECT_URI+SCOPE;
 
-$(document).ready(function () {
-	// autenticação
-	$("#btn-login").click(function(){
-		location.href = URL;
+		location.href=URL;
+		
 	});
-
 });
--- js/sync.js
+++ js/sync.js
@@ -1,23 +1,86 @@
-CLIENT_ID = "367393827472-dm44e1puvndsd9je57ceatdldd05sjju.apps.googleusercontent.com";
-
 $(document).ready(function(){
+	$("#btn").click(function(){
 
-	$("#btn").click(function() {
-		var config = {
-			'client_id': CLIENT_ID,
-			"immediate": true,
-			'scope': 'https://www.google.com/m8/feeds'
+		var param = location.href;
+		param = param.split('#')[1];
+		param = param.split('&')[0];
+
+		var URL = 'https://www.google.com/m8/feeds/contacts/default/full?max-results=99999999&alt=json&'+param;
+
+		var httpRequest = new XMLHttpRequest();
+
+		httpRequest.onreadystatechange = function(){
+			if (httpRequest.readyState === 4) {
+				if (httpRequest.status === 200) {
+					var response = httpRequest.responseText;
+
+					var json = JSON.parse(response);
+
+					var entrys = json.feed.entry;
+
+					entrys.forEach(save);
+				} else {
+					console.log('There was a problem with the request.');
+				}
+			}
 		};
-
-		try{
-			gapi.auth.authorize(config, function(){
-				var token = gapi.auth.getToken();
-				console.log(token);
-			});
-		}catch(exception){
-			console.log("Erro ao obter token");
-			console.log(exception);
-		}
+		httpRequest.open('GET', URL);
+		httpRequest.send();
 	});
 
+	function save(value, index, ar){
+		var nome = value.title.$t;
+		var mail, fone;
+		try{
+			mail = value.gd$mail[0].address;
+		}catch(exception){}
+
+		try{
+			fone = value.gd$phoneNumber[0].$t;
+		}catch(exception){}
+
+		//console.log(nome+" - "+mail+" - "+fone);
+
+		var givenName;
+		var familyName;
+		var nickname;
+
+		if(nome.indexOf(' ') > 0){
+			var array = nome.split(' ');
+			givenName = array[0];
+			familyName = "";
+			nickname = array[0]+" "+array[1];
+			array.forEach(function(value, index, array){
+				if(index > 0){
+					familyName = familyName+" "+value;
+				}
+			});
+		}else{
+			givenName = nome;
+			familyName = "";
+			nickname = nome;
+		}
+
+		var person = new mozContact();
+		person.givenName  = [givenName];
+		person.familyName = [familyName];
+		person.nickname   = [nickname];
+		person.tel = [{
+			carrier: " ",
+			value: fone
+		}];
+		person.email = [{
+			type:["personal"],
+			value: mail
+		}];
+
+		//console.log(person);
+		var saving = navigator.mozContacts.save(person);
+		saving.onsuccess = function(){
+			console.log(nome+" Salvo!");
+		};
+		saving.onerror = function(){
+			console.log("Erro ao salvar "+nome+" !")
+		};
+	}
 });
